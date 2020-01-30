c5ad5354f2bd666b8a87c55fa4c351751781ed9d
leslie <rleslie1015@gmail.com>
2020-01-30 09:47:24 -0600

    latest commit

--- api/visions/visionRouter.js
+++ api/visions/visionRouter.js
@@ -39,7 +39,7 @@ router
             res.status(201).json(post)
         })
         .catch(err => {
-            res.status(500).json(err)
+            res.status(500).json(err)  
         })
 })
 
